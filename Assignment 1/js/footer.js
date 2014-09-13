59de6ed1788dfd439349cd2aff9228f9ce75c481
frostiee22 <mkallo42@gmail.com>
2014-09-13 16:17:22 -0400

    nothing new

--- Assignment 1/js/footer.js
+++ Assignment 1/js/footer.js
@@ -2,7 +2,7 @@
 document.write ('<footer>'+
             '<div class="row">' +
                 '<div class="col-lg-12">' +
-                    '<p>Copyright &copy; Your Website 2015</p>' +
+                    '<p>Copyright &copy; COMP3550 Assignment #1</p>' +
                 '</div>' +
             '</div>' +
         '</footer>');
