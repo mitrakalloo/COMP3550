1056eb9c7a14f1c331cb270811d8f138a2bccced
frostiee22 <mkallo42@gmail.com>
2014-09-13 16:09:29 -0400

    created one menu.js file to be able to edit the menu on all pages from the menu.js

---
+++ Assignment 1/js/footer.js
@@ -0,0 +1,8 @@
+/*global document */
+document.write ('<footer>'+
+            '<div class="row">' +
+                '<div class="col-lg-12">' +
+                    '<p>Copyright &copy; Your Website 2015</p>' +
+                '</div>' +
+            '</div>' +
+        '</footer>');
---
+++ Assignment 1/js/menu.js
@@ -0,0 +1,36 @@
+/*global document */
+document.write ( '<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">' +
+        '<div class="container">' +
+            '<div class="navbar-header">' +
+                '<<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">' +
+                    '<span class="sr-only">Toggle navigation</span>' +
+                    '<span class="icon-bar"></span>' +
+                    '<span class="icon-bar"></span>' +
+                    '<span class="icon-bar"></span>' +
+                '</button>' +
+                '<a class="navbar-brand" href="#">' +
+                        '<ul class="nav navbar-nav" >' +
+                            '<li>' +
+                                '<a href="../Index/index.html">Home</a>' +
+                            '</li>' +
+                    '</ul>' +
+                '</a>' +
+            '</div>' +
+            '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' +
+                '<ul class="nav navbar-nav">'+
+                   '<li>' +
+                        '<a href="../About/about.html">About</a>' +
+                    '</li>' +
+                    '<li>' +
+                      '<a href="../Problems/problems.html">Problems</a>' +
+                    '</li>' +
+                    '<li>' +
+                        '<a href="../Solutions/solutions.html">Solutions</a>' +
+                    '</li>' +
+                    '<li>' +
+                        '<a href="../Contact/contact.html">Contact</a>' +
+                    '</li>' +
+                '</ul>' +
+            '</div>' +
+        '</div>' +
+    '</nav>');
