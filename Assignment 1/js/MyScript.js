5fe86d81dce8212b4e62ce8146d087f00e594b68
frostiee22 <mkallo42@gmail.com>
2014-09-18 20:09:06 -0400

    first Draft of the site

---
+++ Assignment 1/js/MyScript.js
@@ -0,0 +1,17 @@
+function checkPhone(){
+	var data=document.forms["form"]["telephone"].value;
+	console.log(data);
+	if(data.search("-")==-1||data=="")
+	{
+		alert("Contact number required. Contact number can only contain [0-9] and '-'");
+		return true;
+	}
+	for (var i = 0; i <data.length; i++) {
+			if(isNaN(data[i])&&data[i]!='-')
+			{
+				alert("Contact number required. Contact number can only contain [0-9] and '-'");
+				console.log(data);
+				return true;
+			}		
+	};
+}
