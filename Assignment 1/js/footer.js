71b59b60286a049895b2a1512a855224548d8d3e
frostiee22 <mkalloo42@gmail.com>
2014-09-21 00:37:03 -0400

    change the page footer

--- Assignment 1/js/footer.js
+++ Assignment 1/js/footer.js
@@ -1,7 +1,7 @@
 // changes the footer of all pages that have this particular footer
 
 /*global document */
-document.write ('<footer>' +
+document.write('<footer>' +
             '<div class="row">' +
                 '<div class="col-lg-12">' +
                     '<p>Copyright &copy; COMP3550 Assignment #1</p>' +
