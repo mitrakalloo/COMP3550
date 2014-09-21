826609d49b753a9d0fe51fbcd0189937dcfe2cfb
frostiee22 <mkalloo42@gmail.com>
2014-09-21 00:38:42 -0400

    footer changed

--- Assignment 1/js/footer.js
+++ Assignment 1/js/footer.js
@@ -4,7 +4,7 @@
 document.write('<footer>' +
             '<div class="row">' +
                 '<div class="col-lg-12">' +
-                    '<p>Copyright &copy; COMP3550 Assignment #1</p>' +
+                    '<p>Contact us CrimeInc@CrimeInc.com</p>' +
                 '</div>' +
             '</div>' +
         '</footer>');
